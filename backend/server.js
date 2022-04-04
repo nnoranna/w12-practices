96bf1092e8bfa66cfe8b6dc55bafaf7ab8de2b99
nnoranna <anagy.nora.anna@gmail.com>
2022-04-04 17:31:31 +0200

    Create new endpoints, reach data in users.json

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,13 @@ const app = express();
 
 //Next: 
 app.get("/", (req, res, next) => {
-    console.log("Request recieved")
-    res.send("Thank you for your request. This is our esponse");
+    /* console.log("Request recieved")
+    res.send("Thank you for your request. This is our esponse"); */
+    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+})
+
+app.get("/kismacska", (req, res, next) => {
+    res.sendFile(path.join(`${__dirname}/../frontend/somefile.json`));
 })
 
 app.get("/something", (req, res, next) => {
@@ -18,14 +23,26 @@ app.get("/something", (req, res, next) => {
 
 app.get("/api/v1/users", (req, res, next) => {
     console.log("Request recieved on users endpoint")
-    const users = [
+    
+    res.sendFile(path.join(`${__dirname}/../frontend/users.json`));
+
+    /* const users = [
         {
             name: "John",
-            surname: "Doe"
+            surname: "Doe",
+            status: "active"
+        },
+        {
+            name: "Jane",
+            surname: "Done",
+            status: "passive"
         }
     ]
-    res.send(JSON.stringify(users));
-})
+    res.send(JSON.stringify(users)); */
+});
+
+app.use('/public', express.static(`${__dirname}/../frontend/public`));
+
 
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`);
