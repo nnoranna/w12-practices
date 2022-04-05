47d48f09770a5e99dc023f92945d68112cbab3fe
nnoranna <anagy.nora.anna@gmail.com>
2022-04-05 15:41:51 +0200

    Use query and params

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,18 @@ app.get("/api/v1/users", (req, res, next) => {
     console.log("Request recieved on users endpoint")
     
     res.sendFile(path.join(`${__dirname}/../frontend/users.json`));
+});
+
+
+app.get("/api/v1/users-query", (req, res, next) => {
+    console.dir(req.query);
+    console.log(req.query.id);
+    if (req.query.apiKey === "apple") {
+        res.sendFile(path.join(`${__dirname}/../frontend/users.json`));
+    } else {
+        res.send("Unauthorized Request")
+    }
+});
 
 
     /* const users = [
@@ -40,13 +52,43 @@ app.get("/api/v1/users", (req, res, next) => {
         }
     ]
     res.send(JSON.stringify(users)); */   
-});
+
 
 //Create a variable and store the path to reach the users.json
 const userFile = path.join(`${__dirname}/../frontend/users.json`)
 
+/* app.get("/api/v1/users-params/:key", (req, res, next) => {
+    console.dir(req.params);
+    console.log(req.params.key);
+    if (req.params.key === "apple") {
+        res.send("Azt írtad be hogy alma")
+    } else {
+        res.send("Nem azt írtad be hogy alma")
+    }
+}); */
+
+//Active passive
+app.get("/api/v1/users-params/:status", (req, res, next) => {
+    console.dir(req.params);
+    console.log(req.params.status);
+    fs.readFile(userFile, (err, data) => {
+        const users = JSON.parse(data);
+        
+        if (req.params.status === "active") {
+
+            res.send(users.filter(user => user.status === "active"))
+        } else if (req.params.status === "passive") {
+
+            res.send(users.filter(user => user.status === "passive"))
+
+        } else {
+            res.send("Ajjajjj!")
+        }
+    }) 
+});
+
 //Create 2 new endpoints
-app.get("/api/v1/users/active", (req, res, next) => {
+/* app.get("/api/v1/users/active", (req, res, next) => {
     fs.readFile(userFile, (err, data) => {
         if (err) {
             res.send("Something went wrong")
@@ -56,9 +98,9 @@ app.get("/api/v1/users/active", (req, res, next) => {
             res.send(users.filter(user => user.status === "active"))
         }
     })
-});
+}); */
 
-app.get("/api/v1/users/passive", (req, res, next) => {
+/* app.get("/api/v1/users/passive", (req, res, next) => {
     fs.readFile(userFile, (err, data) => {
         if (err) {
             res.send("Something went wrong")
@@ -67,8 +109,7 @@ app.get("/api/v1/users/passive", (req, res, next) => {
             res.send(users.filter(user => user.status === "passive"))
         }
     })
-
-});
+}); */
 
 
 app.use('/public', express.static(`${__dirname}/../frontend/public`));
